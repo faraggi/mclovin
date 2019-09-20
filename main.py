8b34005e71a43a51efcc4e16f68241ed9b119e0c
Felipe Faraggi <felipefaraggi@gmail.com>
2019-09-20 17:49:12 +0200

    WIP. still work to do on the business logic of this spaghetti script
    Signed-off-by: Felipe Faraggi <felipefaraggi@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os, json
+import os, json, sys
 
 import requests
 from github import Github
@@ -9,48 +9,90 @@ from gitterpy.client import GitterClient
 # access tokens
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
+g = Github(GITHUB_TOKEN)
 
-# test data
+ORGS = []
+# PROJECTS = {}
+PROJECTS = eval(open("test.json").read())
+LICENSES = eval(open("licenses").read())
+
+# test data and real data
 with open('test.json') as test_json: 
     test_data = json.load(test_json) 
 
-test_data_projects = test_data['data']['allProjects']
+project_list = PROJECTS['data']['allProjects']
 
+# print(project_list)
+# print(len(project_list))
 
-g = Github(GITHUB_TOKEN)
+# cleaning up project list
+for item in project_list:
+    name = item['name']
+    # print(name)
+    # removing projects without github link
+    if not item['githubLinks']:
+        project_list.remove(item)
 
-# for repo in g.get_user().get_repos():
-project_list = []
-for project in test_data_projects:
-    print(project)
-    if len(project['githubLinks']) > 0:
-        project_list.append(project)
+# for project in project_list:
+#     # getting projects with multiple github links
+#     if len(item['githubLinks']) > 1:
+#         for link in item['githubLinks']:
+#             project_list[name] = link
 
-for project in test_data_projects:
+print(project_list)
+print(len(project_list))
+print('---')
+
+for project in project_list:
     print(project)
     # print(project['githubLinks'])
     # try org
     try:
         name = g.get_organization(project['name'])
-        # print(f"{name} is ORG!")
     except UnknownObjectException:
-        # print("UnknownObjectException")
-        pass
-    
-    # try project
-    # try:
-    #     name = g.get_project(project['name'])
-    #     print(f"{name} is PROJECT!")
-    # except UnknownObjectException:
-    #     print("NOT PROJECT")
+        # try user
+        try:
+            name = g.get_repo(project['name'])
+        except UnknownObjectException:
+            # try repo
+            try:
+                name = g.get_user(project['name'])
+                # print(f"{name} is USER!")
+            except UnknownObjectException:
+                # print(name)
+                print("UnknownObjectException")
+                # print("Isn't any type.")
+                project_list.remove(project)
 
-    # try user
-    try:
-        name = g.get_user(project['name'])
-        # print(f"{name} is USER!")
-    except UnknownObjectException:
-        # print("UnknownObjectException")
-        pass
+print('---')
+print(project_list)
+print(len(project_list))
+
+# for i, k in json_list.items():
+#     print(i, "-", k)
+
+# print(json_list)
+
+# cleaning up project list
+# json_list = {}
+# for item in (real_data or test_data_projects):
+#     name = item['name']
+#     # print(name)
+#     # getting projects with multiple github links
+#     if len(item['githubLinks']) > 1:
+#         for link in item['githubLinks']:
+#             json_list[name] = link
+# 
+#     # removing projects without github link
+#     elif item['githubLinks']:
+#         json_list[item['name']] = item['githubLinks']
+
+# for i, k in json_list.items():
+#     print(i, "-", k)
+# 
+# print(json_list)
+
+
 
 # print("+++++++++++++++++++++++++++++++")
 # print("+++++++++++++++++++++++++++++++")
